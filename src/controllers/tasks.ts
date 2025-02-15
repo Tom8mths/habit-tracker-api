bbbdf5ea449303cdda125e4429a51577029dd1b6
Matheus Satelos <matheus.satelos@gmail.com>
2025-02-14 22:54:43 -0300

    remove console logs

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -6,8 +6,6 @@ export const getAllTasks = async (req: Request, res: Response): Promise<void> =>
   try {
     const currentUserId = get(req, 'identity._id') as string;
     const tasks = await getTasks(currentUserId);
-    console.log(tasks);
-    
 
     res.status(200).json(tasks);
   } catch (error) {
@@ -20,10 +18,7 @@ export const createNewTask = async (req: Request, res: Response): Promise<void> 
   try {
     const currentUserId = get(req, "identity._id") as string;
     const values = req.body;
-    console.log('values', values);
-    console.log('currentUserId', currentUserId);
-    
-    
+
     const newTask =  await createTask(values, currentUserId);
     res.status(200).json(newTask).end();
   } catch (error) {
